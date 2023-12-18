24d2edd37d9d73c2d249cd0fb8958521e4967466
Carlos Encinar Méndez <7861015@alumnos.ufv.es>
2023-12-18 13:41:52 +0100

    Añadido url opinion

--- relecloud/urls.py
+++ relecloud/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('destination/<int:pk>', views.DestinationDetailView.as_view(), name='destination_detail'),
     path('cruise/<int:pk>', views.CruiseDetailView.as_view(), name='cruise_detail'),
     path('info_request', views.InfoRequestCreate.as_view(), name='info_request_create'),
-    path('opinions/', views.OpinionsView.as_view(), name='opinions'),
+    path('opinions/', views.OpinionsView.as_view(), name='opinions'), #Añadido el url de opinions
 
 ]
+
