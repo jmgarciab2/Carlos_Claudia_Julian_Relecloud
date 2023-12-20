b053d4ee460aa53cf2edf34d794706531f3ca5f0
clauhuerta <claudiahr02@icloud.com>
2023-12-20 18:28:03 +0100

    funcionalidad completa

---
+++ relecloud/javascript/funcionesCorreo.js
@@ -0,0 +1,20 @@
+function sendEmail() {
+    // Configura la información del servidor SMTP
+    Email.server = {
+        Host: "smtp.office365.com",
+        Username: "pedri_julian@outlook.es",
+        Password: "Constan12",
+        Port: 587,
+    };
+
+    // Configura el correo electrónico que se enviará
+    Email.send({
+        To: 'correoprueba@example.com',
+        From: "pedri_julian@outlook.es",
+        Subject: "Confirmación Solicitud",
+        Body: "Se ha aprobado con éxito su solicitud!",
+    }).then(
+        message => alert("Email sent successfully: " + message),
+        error => alert("Error sending email: " + error)
+    );
+}
