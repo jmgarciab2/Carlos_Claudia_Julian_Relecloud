d8634637b3aa474b1deedc915887229e2b62be36
Julian <jmgarciab2@gmail.com>
2023-12-28 13:27:31 +0100

    Added config files to show media on web

--- relecloud/urls.py
+++ relecloud/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -12,3 +13,5 @@ urlpatterns = [
     path('opinions/', views.OpinionsView.as_view(), name='opinions'), #Añadido el url de opinions
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
